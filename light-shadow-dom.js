4c355778e2b77d8f3874d33ffe72f535b9c0e296
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-02-13 14:55:56 +1000

    theme based button

--- light-shadow-dom.js
+++ light-shadow-dom.js
@@ -1,5 +1,21 @@
 const template = document.createElement("template");
 template.innerHTML = `
+<style>
+:host([theme="error"]) {
+    --button-background-color: red;
+    --button-text-color: white;
+}
+
+:host([theme="warn"]) {
+    --button-background-color: yellow;
+    --button-text-color: black;
+}
+
+.pink {
+    background-color: var(--button-background-color);
+    color: var(--button-text-color);
+}
+</style>
 <slot></slot>
 <label>Shadow DOM Text</label>
 <button class="pink">Shadow DOM Button</button>
